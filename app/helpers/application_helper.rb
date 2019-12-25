7030089edf617939e81798da89c499c9a768a03e
Daniel <adef96@mail.ru>
2019-12-25 17:27:52 +0300

    Added some new stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(samp_title = "")
+    base_title = "Sample shit"
+    if samp_title.empty?
+      return base_title
+    end
+    return samp_title + " | " + base_title
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/help', to: 'sample#help'
   get '/sign_up', to: 'users#new'
   root 'sample#home'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
