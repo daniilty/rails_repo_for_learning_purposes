90b1c932652e15096e888049ad74aae4e71b50fb
Daniel <adef96@mail.ru>
2020-01-03 16:37:51 +0300

    Added base functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   def show
     @user = User.find(params[:id])
+  end
+  def create
+    params_user = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'session/new'
   get 'users/new'
   get '/home' , to: 'sample#home'
   get '/about', to:'sample#about'
   get '/help', to: 'sample#help'
   get '/sign_up', to: 'users#new'
+  post '/sign_up', to: 'users#create'
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
   root 'sample#home'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
